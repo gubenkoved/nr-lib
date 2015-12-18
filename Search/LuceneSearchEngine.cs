a4a9c0b2509669f0df11432000da9c0369efbc12
Eugene Gubenkov <gubenkoved@gmail.com>
2015-12-18 22:29:44 +0300

    Added stub for Lucene search.

---
+++ Search/LuceneSearchEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRLib.Search
+{
+    public class LuceneSearchEngine : SearchEngine
+    {
+        public override event ErrorHandler Error;
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<MatchInfo> Search(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
